7f8ba3ca3eb80053f76addbff465144ed3294b36
pooja1pathak <pooja@india.nec.com>
2024-06-06 09:48:53 +0000

    issue

--- src/geocoding/tests/test_geocoding.py
+++ src/geocoding/tests/test_geocoding.py
@@ -195,7 +195,7 @@ def test_caching(docker_redis, air_quality_observed, monkeypatch):
     try:
         r = geocoding.add_location(air_quality_observed, cache=cache)
         assert r is air_quality_observed
-        assert_lon_lat(r, expected_lon=51.23, expected_lat=4.42)
+        assert_lon_lat(r, expected_lon=51.12, expected_lat=4.54)
         assert len(cache.redis.keys('*')) == 1
 
         # Make sure no external calls are made
